2953881fee6ae9942dc54fdd8e529992ba98316b
Greg <firepaws@gmail.com>
2018-09-11 21:50:49 -0500

    More functions

--- 450/HW01.hs
+++ 450/HW01.hs
@@ -16,7 +16,7 @@
 
 module HW01
    (
-	prodSqSmall, xor      
+	prodSqSmall, xor, implies, ccArea, addTax, subTax
     )
 
 where
@@ -46,5 +46,49 @@ implies  a b
   | a == True && b == True = True
   | otherwise = False
 
+-- ELIFP Ch. 5, Ex. 7
+-- Started using Template build out here. 
+ccArea :: Double -> Double -> Double
+ccArea a b 
+  | a < 0 || b < 0 = error ""
+  | a < b = ccArea b a
+  | otherwise = ((a/2)^2 * pi) - ((b/2)^2 * pi) 
+    
+-- ELIFP Ch. 5, Ex. 9
+-- Consider the equation:  addTax c p = ct
+addTax :: Double -> Double -> Double
+addTax a b 
+  | a < 0 || b < 0  = error ""
+  | otherwise = ((a * b) /100) + a
 
- 
+subTax :: Double -> Double -> Double
+subTax a b 
+  | a < 0 || b < 0 = error ""
+  | otherwise = (a / (1 + ( b /100)))
+
+-- ELIFP Ch. 5, Ex.11
+
+-- Using proleptic Gregorian calendar, which underlies ISO 8601 standard
+-- (month, day, year)
+feb :: (Int,Int,Int) -> Bool
+feb (m,d,y)
+  | y mod 2 == 1 = d <= 28
+  | y mod 400 == 0 = d <= 29
+  | y mod 100 == 0 = d <= 28
+  
+
+  
+validMonth :: (Int, Int, Int) -> Bool
+validMonth (m,d,y)
+  | m == 2 = feb (m,d,y)
+  | otherwise = True
+
+validDay :: (Int,Int,Int) -> Bool
+validDay (m,d,y) 
+  | m < 0 || m > 12 = error
+  | d < 28 || d > 31 = error
+  
+
+-- -- ELIFP Ch. 5, Ex. 12
+-- -- Roman numerals in range [0-3999], where 0 represented as empty string
+-- roman :: Int -> String
