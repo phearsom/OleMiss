fadd8a4c89bedf5c9255e549f6c939cfb4c0e1dc
Greg <firepaws@gmail.com>
2018-09-24 23:34:15 -0500

    Changes to be committed: 	modified:   UAC.sh

--- CSCI323/Scripts/UAC.sh
+++ CSCI323/Scripts/UAC.sh
@@ -7,12 +7,14 @@ Date=`date +%y%m%d`
 
 while IFS=, read field1 field2 field3 field4 field5 field6
 do
-	`tr _ " " < $field2 > $field2 `
-	`adduser --gecos --disabled-password`
-
 	
-	chpasswd <<< "$field1:$field1+$Date"
-
+	`adduser --gecos $field1 $field2 $field3 $field4 $field5 $field6 --disabled-password`
+	fullName="$(echo ${field2} | tr -s '_' ' ')"   
+#	echo $field1 $field2 $field3 $field4 $field5 $field6 $field7
+	pass="$field1""$Date"	
+	echo $field1:$pass | chpasswd
+	echo $pass
+	echo $field1	
 done < empRec.csv 
 
 
