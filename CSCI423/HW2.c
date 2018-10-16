cab099292329949b8cfe2e9511899fb145ab1ebc
Greg <firepaws@gmail.com>
2018-10-16 11:31:11 -0500

    HW2

---
+++ CSCI423/HW2.c
@@ -0,0 +1,59 @@
+/* 
+    CSCI 423
+    Homework 2 
+    Collatz Conjecture
+    Gphillip - 10678899
+    Gregory Phillips
+
+*/
+
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char * argv[])
+{
+
+	if(argc != 2){
+        
+        fprintf(stderr , "Usage: ./a.out <starting value>\n");
+        
+        return -1;
+    }
+    int num = atoi(argv[1]);
+        if(num <= 0 ){
+            fprintf(stderr, "<starting value> should be a positive integer\n");
+            return -1;
+        }
+	
+	pid_t pid;
+        
+        pid = fork();
+
+		if (pid == 0)
+		{
+			
+			printf("%d, ",num);
+			while (num != 1)
+			{
+				if (num %2 == 0)
+				{
+					num = num / 2;
+				}
+				else if (num % 2 == 1)
+				{
+					num = 3 * (num) + 1;
+				}	
+			
+				printf("%d, ",num);
+			}
+		
+			printf("\n");
+		}
+		else
+		{
+			wait();
+		}
+	return 0; 
+}
