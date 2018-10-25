3331e6489e9e80f92a61734e1bf60027185e49ae
Greg <firepaws@gmail.com>
2018-10-25 11:39:44 -0500

    HW3

---
+++ CSCI423/HW3.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *avg_func(void *str);
+void *min_func(void *ptr);
+void *max_func(void *ptr);
+
+double average;
+int smallest;
+int largest;
+
+typedef struct numbers
+{
+    int size;
+    int * values;
+}numbers;
+
+int main(int argc, char *argv[])
+{
+    if(argc == 1){
+        fprintf(stderr, "Usage: ./a.out <at least one integer as input>\n");
+        return(-1);
+	}
+
+    int i = 0;
+    int copy[argc-1];
+    for(i; i < (argc -1); i++)
+    {
+        copy[i] = atoi(argv[i+1]);
+    }
+
+    pthread_t thread1, thread2, thread3;
+    int  t1, t2, t3;
+
+    numbers ds = {argc - 1, copy};
+
+    t1 = pthread_create(&thread1, NULL, (void *) avg_func, (void *) &ds);
+    if(t1)
+    {
+        fprintf(stderr,"Error - pthread_create() return code: %d\n", t1);
+        exit(EXIT_FAILURE);
+    }
+
+    t2 = pthread_create(&thread2, NULL, (void *) min_func, (void *) &ds);
+    if(t2)
+    {
+        fprintf(stderr,"Error - pthread_create() return code: %d\n",t2);
+        exit(EXIT_FAILURE);
+    }
+
+    t3 = pthread_create(&thread3, NULL, (void *) max_func, (void *) &ds);
+    if(t3)
+    {
+        fprintf(stderr,"Error - pthread_create() return code: %d\n", t3);
+        exit(EXIT_FAILURE);
+    }
+
+
+
+  
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    pthread_join(thread3, NULL);
+
+    printf("The average value is:  %g\n", average);
+    printf("The minimum value is:  %d\n", smallest);
+    printf("The maximum value is:  %d\n", largest);
+
+    exit(EXIT_SUCCESS);
+}
+
+void *avg_func(void *ptr)
+{
+    numbers * copy;
+    copy = (numbers *) ptr;
+
+    int sz = copy->size;
+    int i;
+
+    for(i = 0; i < sz; i++)
+    {
+        average += (copy->values[i]);
+    }
+    average = (int)(average
+ / sz);
+}
+
+void *min_func(void *ptr)
+{
+    numbers * copy;
+    copy = (numbers *) ptr;
+
+    int sz = copy->size;
+    int i;
+
+    smallest = (copy->values[0]);
+    for(i = 1; i < sz; i++)
+    {
+        if(smallest > (copy->values[i]))
+        {
+            smallest = (copy->values[i]);
+        }
+    }
+}
+
+void *max_func(void *ptr)
+{
+    numbers * copy;
+    copy = (numbers *) ptr;
+
+    int sz = copy->size;
+    int i;
+
+    largest = copy->values[0];
+
+    for(i = 1; i < sz; i++)
+    {
+        if(largest < copy->values[i])
+        {
+            largest = copy->values[i];
+        }
+    }
+}
---
+++ CSCI423/disc8.c
@@ -0,0 +1,57 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int sum; /* this data is shared by the thread(s) */
+void *runner(void *param); /* threads call this function */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		fprintf(stderr,"usage: a.out <integer value>\n");
+		return -1;
+	}
+
+	int N = atoi(argv[1]);
+
+	if (N < 0) {
+		fprintf(stderr,"%d must be >= 0\n",atoi(argv[1]));
+		return -1;
+	}
+
+	pthread_t tid[N]; /* the thread identifier */
+	pthread_attr_t attr; /* set of thread attributes */
+
+	int i;
+
+	sum = 0;
+
+	/* get the default attributes */
+	pthread_attr_init(&attr);
+
+	/* create the thread */
+	for (i=0; i<N; i++)
+		pthread_create(&tid[i],&attr,runner,(void *)i);
+
+	/* wait for the thread to exit */
+	for (i=0; i<N; i++)
+		pthread_join(tid[i],NULL);
+
+	printf("sum = %d\n",sum);
+}
+
+/* The thread will begin control in this function */
+void *runner(void *param)
+{
+	int thread_no = (int) param;
+	int i;
+
+	for (i=0; i<1000; i++)
+	{
+		sum++;
+		sum--;
+	}
+
+	printf("This is thread %d, I have increased the sum value by 1 and then decreased the sum value by 1.\n", thread_no);
+
+	pthread_exit(0);
+}
